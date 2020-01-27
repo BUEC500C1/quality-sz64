984807eb764f223dd1721b6f50d29919b79b037e
Steven <sz64@bu.edu>
2020-01-27 16:46:07 -0500

    added invalid input checking

--- arabic2roman.py
+++ arabic2roman.py
@@ -1,17 +1,14 @@
 def arabic2roman(aNum):
-    aNums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    rNums = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-    rNum = '';
-    i = 0;
-    while aNum > 0:
-        for _ in range(aNum//aNums[i]):
-            rNum += rNums[i];
-            aNum -= aNums[i];
-        i += 1;
+	if (isinstance(aNum, int) & (0 < aNum < 4000)):
+		aNums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+		rNums = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+		rNum = '';
+		i = 0;
+		while aNum > 0:
+			for _ in range(aNum//aNums[i]):
+				rNum += rNums[i];
+				aNum -= aNums[i];
+			i += 1;
+	else:
+		return 'Error';
     return rNum;
-
-# while(True):
-	# print('Enter a Arabic Number: ');
-	# aNum = input();
-    # rNum = arabic2roman(aNum);
-	# print(rNum);
--- test_arabic2roman.py
+++ test_arabic2roman.py
@@ -1,6 +1,11 @@
 import arabic2roman as a
 
-def test_test():
+def test_valid():
     aNum = 59;
     rNum = a.arabic2roman(aNum);
     assert rNum == 'LIX';
+	
+def test_invalid():
+    aNum = 'string';
+    rNum = a.arabic2roman(aNum);
+    assert rNum == 'Error';
