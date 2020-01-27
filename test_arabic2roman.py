4c992c0cb530e724e65347f0aa2aef36e41c0d88
Steven <sz64@bu.edu>
2020-01-27 17:02:53 -0500

    indentation and spacing errors fixed in test file

--- test_arabic2roman.py
+++ test_arabic2roman.py
@@ -1,7 +1,7 @@
 import arabic2roman as a
 
 def test_valid():								# Checks values within the valid range
-    assert a.arabic2roman(1) == 'I';
+	assert a.arabic2roman(1) == 'I';
 	assert a.arabic2roman(4) == 'IV';
 	assert a.arabic2roman(9) == 'IX';
 	assert a.arabic2roman(59) == 'LIX';
@@ -13,7 +13,7 @@ def test_valid():								# Checks values within the valid range
 	assert a.arabic2roman(3999) == 'MMMCMXCIX';
 	
 def test_invalid():
-    assert a.arabic2roman('string') == 'Error'; # Only integers are valid
+	assert a.arabic2roman('string') == 'Error'; # Only integers are valid
 	assert a.arabic2roman(-1) == 'Error';		# Current function only works with values 1 to 3999
 	assert a.arabic2roman(0) == 'Error';
 	assert a.arabic2roman(4000) == 'Error';
