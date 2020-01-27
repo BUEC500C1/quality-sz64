28089f0c321b267aab6b889e4f779729f45acd8b
Steven <sz64@bu.edu>
2020-01-27 17:00:14 -0500

    added more tests

--- test_arabic2roman.py
+++ test_arabic2roman.py
@@ -1,11 +1,19 @@
 import arabic2roman as a
 
-def test_valid():
-    aNum = 59;
-    rNum = a.arabic2roman(aNum);
-    assert rNum == 'LIX';
+def test_valid():								# Checks values within the valid range
+    assert a.arabic2roman(1) == 'I';
+	assert a.arabic2roman(4) == 'IV';
+	assert a.arabic2roman(9) == 'IX';
+	assert a.arabic2roman(59) == 'LIX';
+	assert a.arabic2roman(94) == 'XCIV';
+	assert a.arabic2roman(495) == 'CDXCV';
+	assert a.arabic2roman(999) == 'CMXCIX';
+	assert a.arabic2roman(1010) == 'MX';
+	assert a.arabic2roman(1489) == 'MCDLXXXIX';
+	assert a.arabic2roman(3999) == 'MMMCMXCIX';
 	
 def test_invalid():
-    aNum = 'string';
-    rNum = a.arabic2roman(aNum);
-    assert rNum == 'Error';
+    assert a.arabic2roman('string') == 'Error'; # Only integers are valid
+	assert a.arabic2roman(-1) == 'Error';		# Current function only works with values 1 to 3999
+	assert a.arabic2roman(0) == 'Error';
+	assert a.arabic2roman(4000) == 'Error';
